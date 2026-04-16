22441e1f79e08f322487bf40d1b85760d0335f0d
sv3895-commits <sv3895@srmist.edu.in>
2026-04-16 18:55:30 +0530

    Coded UC5: Hello with multiple args using enhanced for loop

---
+++ HelloAppUC5.java
@@ -0,0 +1,21 @@
+public class HelloAppUC5 {
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } 
+        else {
+            StringBuilder names = new StringBuilder();
+
+            for (String name : args) {
+                if (names.length() > 0) {
+                    names.append(", ");
+                }
+                names.append(name);
+            }
+
+            System.out.println("Hello, " + names + "!");
+        }
+    }
+}
